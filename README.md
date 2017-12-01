60db2e58ea4ed900d1da85b711aff2b900312dc0
leesipeng <leesipeng@gmail.com>
2017-12-01 19:44:57 +0800

    delete blog

--- README.md
+++
@@ -1,5 +0,0 @@
-# blog-demo
-
-> 基于Node.js
-
-
--- actions/logout.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let indexAction = require('./index');
-
-module.exports = function(req,res) {
-    req.session.isLogined = false;
-    indexAction(req,res);
-
-};
