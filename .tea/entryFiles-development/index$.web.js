2535d0cf64706a15abfde1d97567b00367680f86
leesipeng <leesipeng@gmail.com>
2017-12-01 21:26:02 +0800

    update

---
+++ .tea/entryFiles-development/index$.web.js
@@ -0,0 +1,3 @@
+import './config$';
+import '../..//pages/todos/todos';
+import '../..//pages/add-todo/add-todo';
---
+++ .tea/entryFiles-development/index$.worker.js
@@ -0,0 +1,5 @@
+import './config$';
+
+import '../..//app';
+import '../..//pages/todos/todos';
+import '../..//pages/add-todo/add-todo';
