fc6560d39d1e67bce265a5bc760f4d8ca7b4650c
leesipeng <leesipeng@gmail.com>
2017-12-04 22:37:30 +0800

    add swiper

--- src/pages/find/find.js
+++ src/pages/find/find.js
@@ -1,13 +1,39 @@
 // 获取全局应用程序实例对象
 // const app = getApp()
-
+var Zan = require('../../components/zanUI/index');
 // 创建页面实例对象
-Page({
+Page(Object.assign({}, Zan.Tab, {
   /**
    * 页面的初始数据
    */
   data: {
-    title: 'find'
+    title: 'find',
+    tab3: {
+      list: [{
+        id: 'music',
+        title: '音乐'
+      }, {
+        id: 'video',
+        title: '视频'
+      }, {
+        id: 'radio',
+        title: '电台'
+      }],
+      selectedId: 'music',
+      scroll: false,
+      // height: 45
+    },
+    slider: [
+      {
+        picUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512407672880&di=c4b61281bbb875d282baa4235426be73&imgtype=0&src=http%3A%2F%2Fimg2.niutuku.com%2Fdesk%2F1208%2F1307%2Fntk-1307-6459.jpg'
+      },
+      {
+        picUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2222457580,3572532973&fm=27&gp=0.jpg'
+      },
+      {
+        picUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1639473443,2804235162&fm=27&gp=0.jpg'
+      },
+    ]
   },
 
   /**
@@ -50,5 +76,13 @@ Page({
    */
   onPullDownRefresh () {
     // TODO: onPullDownRefresh
+  },
+  handleZanTabChange(e) {
+    var componentId = e.componentId;
+    var selectedId = e.selectedId;
+
+    this.setData({
+      [`${componentId}.selectedId`]: selectedId
+    });
   }
-})
+}))
